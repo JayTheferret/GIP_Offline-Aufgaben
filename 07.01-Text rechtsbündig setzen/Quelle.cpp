8b5c6316a3b4c08f290b9853b459ae6a0a9896d8
Jay <57915168+JayTheferret@users.noreply.github.com>
2019-12-11 13:01:24 +0100

    initial commit

--- 07.01-Text rechtsbündig setzen/Quelle.cpp
+++ 07.01-Text rechtsbündig setzen/Quelle.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
 
 	string text[4];
+	int a[4];
 
 	for (int i = 0; i < 4; i++) { //max 4 Eingaben zulassen
 		cout << "Textzeile = ? ";
@@ -16,12 +17,49 @@ int main() {
 		}
 	}
 
-	int a[4];
+	//Länge in a einspeichern
+
+	int i = 0;
+
+	for (int j = 0; j < 4; j++) {
+		a[j] = text[i].length();
+		i++;
+	}
+
+	//längste Zeile ermitteln
+
+	string lang;
+
+	if (a[0] > a[1] && a[0] > a[2] && a[0] > a[3]) {
+		lang = text[0];
+	}
+	else if (a[1] > a[0] && a[1] > a[2] && a[1] > a[3]) {
+		lang = text[1];
+	}
+	else if (a[2] > a[0] && a[2] > a[1] && a[2] > a[3]) {
+		lang = text[2];
+	}
+	else {
+		lang = text[3];
+	}
+
+	//Ausgabe
 
 	for (int i = 0; i < 4; i++) {
-		text[i].length() = a[i];
 
+		if (text[i].length() >= 1) {
 
-		cout << text[0] << "länge" << a[0];
+			//solange text noch kürzer als längster, mit # auffüllen
+
+			while (text[i].length() < lang.length()) {
+				text[i] = "#" + text[i];
+			}
+			cout << text[i] << endl;
+		}
 	}
+
+	system("PAUSE");
+	return 0;
 }
+
+	

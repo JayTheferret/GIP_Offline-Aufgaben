c5ae1973d4d0a5b3d2f0e5db1f9b3d66defe9d2b
Jay <57915168+JayTheferret@users.noreply.github.com>
2019-12-12 13:29:26 +0100

    initial commit

---
+++ 03.03-Zahlziffer-Umwandlung(F)/Quelle.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	char input = 'a';
+
+	cout << "Bitte geben Sie das Zeichen ein: ? ";
+	cin >> input;
+
+	if (input == 'e') {
+		cout << "Das Programm beendet sich jetzt. " << endl;
+	}
+
+	else if (input >= '0' && input <= '9') {
+		int inputz = input - 48;
+		cout << inputz << " + 5 = " << inputz + 5 <<endl;
+	}
+
+	else {
+		cout << "Weder 'e' noch Ziffer " << endl;
+	}
+
+	system("PAUSE");
+	return 0;
+}

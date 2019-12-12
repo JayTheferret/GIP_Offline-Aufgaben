ec2f8fc2eda986b95b259908e3b4452bd2d65811
Jay <57915168+JayTheferret@users.noreply.github.com>
2019-12-12 14:14:05 +0100

    added 03.06

---
+++ 03.06-Kleinste und Größte von drei eingegebenen Zahlen(F)/Quelle.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int input[3];
+
+	for (int i = 0; i < 3; i++) {
+		cout << "Bitte geben Sie die " << i + 1 << ". Zahl ein: ? ";
+		cin >> input[i];
+	}
+
+	int g = input[0];
+	int k = input[0];
+
+	for (int i = 0; i < 3; i++) {
+
+		if (input[i] > g) {
+			g = input[i];
+		}
+		else {
+			k = input[i];
+		}
+	}
+
+	cout << "Die kleinste eingegebene Zahl lautet: " << k << endl;
+	cout << "Die groesste eingegebene zahl lautet: " << g << endl;
+
+	system("PAUSE");
+	return 0;
+}

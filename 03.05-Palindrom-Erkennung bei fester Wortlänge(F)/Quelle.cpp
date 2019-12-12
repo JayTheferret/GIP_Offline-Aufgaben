5a192f445f2fd42fdd507dcebebb3adb57f0b2ef
Jay <57915168+JayTheferret@users.noreply.github.com>
2019-12-12 13:54:26 +0100

    initial commit

---
+++ 03.05-Palindrom-Erkennung bei fester Wortlänge(F)/Quelle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string input = " ";
+
+	cout << "Bitte geben Sie die Zeichenkette (6 Zeichen) ein: ? ";
+	cin >> input;
+
+	if (input[0,1,2] == input[5,4,3]) {
+		cout << "Das eingegebene Wort ist ein Palindrom. " << endl;
+	}
+
+	else {
+		cout << "Das eingegebene Wort ist KEIN Palindrom. " << endl;
+	}
+
+	system("PAUSE");
+	return 0;
+}

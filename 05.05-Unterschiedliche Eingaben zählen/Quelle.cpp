6a918b3351a5037b84696bf058e1f9e10575b285
Jay <57915168+JayTheferret@users.noreply.github.com>
2019-12-26 21:53:24 +0100

    solved 5.05

--- 05.05-Unterschiedliche Eingaben zählen/Quelle.cpp
+++ 05.05-Unterschiedliche Eingaben zählen/Quelle.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 
 	int Zahlen[9];
-	int uz [6];
+	int uz[6] = { 0 };
 	int UZ = 0;
 
 	for (int i = 0; i < 9; i++) {
@@ -15,18 +15,24 @@ int main() {
 		do {
 			cout << "Bitte geben Sie die " << i + 1 << ". Zahl ein: ? ";
 			cin >> Zahlen[i];
-		} while (Zahlen[i] < 1 || Zahlen[i] > 6);
 
-		for (int j = 0; j < 6; j++) {
-			if (Zahlen[i] == j + 1) {
-				uz[j] += 1;
+		} while (Zahlen[i] < 1 || Zahlen[i] > 6);
+	}
+
+	for (int j = 0; j < 6; j++) {// zahlenwerte 1 bis 6 durchgehen
+
+		for (int i = 0; i < 9; i++) {//eingegebene Zahlen durchlaufen
+
+			if (Zahlen[i] == j+1) {//eingegebene Zahlen mit Zahlenwerten vergleichen
+				uz[j] += 1; //wenn wert vorhanden -> Vorkommen um eins erhöhen
 			}
-		}
+		}	
 	}
 
 	for (int j = 0; j < 6; j++) {
-		if (uz[j] > 0) {
-			UZ += 1;
+
+		if (uz[j] > 0) { // wen zahlenvorkommen über 0
+			UZ += 1; //verschiedene Vorkommen ^1
 		}
 	}
 
